a670dfd0c91f153345cfdaa20354ca1f9a8d96d5
Shashi-Chaurasia <shashichaurasia886@gmail.com>
2021-06-01 01:18:43 +0530

    try and expection handling

---
+++ 01_try.py
@@ -0,0 +1,14 @@
+while(True):
+    print("press q for quit : ")
+    user = input("Enter your number : ")
+    if user == "q":
+        break
+    try:
+        user = int(user)
+        if user < 6:
+            print("plz enter greater number :crossed_swords: ")
+
+    except Exception as e:
+        print(f"Invalid input {e}")
+
+print("Thanks for playing this game 😄")
